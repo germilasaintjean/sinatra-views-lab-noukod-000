f9adca86c4432489ba4e090a42a249a76d825748
Saint-Jean Germila <germila@noukod.com>
2019-01-14 20:08:32 +0000

    Automatically backed up by Learn

---
+++ app.rb
@@ -0,0 +1,21 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+get '/' do
+	erb :index
+end
+
+get '/goodbye'do
+	erb :goodbye
+end
+
+get '/hello'do
+	erb :date
+end
+
+get '/date'do
+	erb :date
+end
+
+end
